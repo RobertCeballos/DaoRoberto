3b7cee82a7007c1d50d306152a385bbb20e8c9a9
roberacc <roberacc@s5pc32.eisc.univalle.edu.co>
2012-06-05 11:42:03 -0500

    jun05

---
+++ DaoRobert/src/DAO/Fachada.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author roberacc
+ */
+public class Fachada {
+    
+}
